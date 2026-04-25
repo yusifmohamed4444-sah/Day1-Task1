ed442fd857dd41c917264f7511ff903a53082837
yuossefmohamed7777 <yussif.mohamed7777@gmail.com>
2026-04-25 20:48:09 +0300

    Add project files.

---
+++ Islam's Carpet Cleaning Service/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Islam_s_Carpet_Cleaning_Service
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int priceSmall = 25;
+            int priceLarge = 35;
+            double taxRet = 0.06;
+
+            Console.WriteLine("please, enter the Number of Small carpet  : ");
+            int small = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("please, enter the Number of Large carpet  : ");
+            int large = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("The Price small room :$25\nThe Price Large room :$35");
+            Console.WriteLine("The Price small room :$25");
+            Console.WriteLine("The Price Large room :$35");
+
+
+            int cost = (small * priceSmall) + (large * priceLarge);
+            double tax = cost * taxRet;
+            double totalCost = tax + cost;
+
+
+            Console.WriteLine("the Cost :$ " + cost);
+            Console.WriteLine("the Tax :$ " + tax);
+            Console.WriteLine("======================================");
+            Console.WriteLine("the Total estimate :$ " +totalCost);
+            Console.WriteLine("This estimate is valid for 30 days ");
+
+        }
+    }
+}
